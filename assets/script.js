07909848c4bcd18cdfdd9d309116733d75bf2374
Stephen Price <stephen_lprice@live.com>
2021-01-02 23:35:55 -0600

    password generation and resetting is working

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@
 
 // Document IDs & classes
 var generateBtn = document.querySelector("#generate");
+var writeBtn = document.querySelector("#inputWrite");
 var pCriteria = document.querySelector("#criteria");
 var inputLow = document.querySelector("#inputLow");
 var inputUp = document.querySelector("#inputUp");
@@ -41,11 +42,6 @@ function charConcat(l,u,n,s) {
   if (s === true) {
     charSum += charSpec;
   }
-  if (l === false && u === false && n === false && s === false) {
-    console.log("ERROR: invalid inputs, please select at least 1 character type to generate a password");
-    var passConsole = document.querySelector("#password");
-    passConsole.value = "ERROR: invalid inputs, please select at least 1 character type to generate a password";
-  }
   return charSum;
 }
 
@@ -68,10 +64,16 @@ function generatePassword () {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  if (critLow === false && critUp === false && critNum === false && critSpec === false) {
+    console.log("ERROR: invalid inputs, please select at least 1 character type to generate a password");
+    var passConsole = document.querySelector("#password");
+    passConsole.value = "ERROR: invalid inputs, please select at least 1 character type to generate a password";
+  }
+  else {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 }
 
 // Display the password criteria form
@@ -81,7 +83,7 @@ function showCriteria() {
   passConsole.value = "Please select at least 1 character type and choose a password length";
 }
 
-// Uses checkbox values to alter password criteria booleans
+// Uses checkbox & range inputs to alter password criteria values or resets them
 function inputCheck() {
   if (this === inputLow) {
     critLow = document.querySelector("#inputLow").checked;
@@ -103,11 +105,14 @@ function inputCheck() {
     passLength = document.querySelector("#inputLen").value;
   }
   else if (this === pCriteria) {
-    critLow = document.querySelector("#inputLow").checked;
-    critUp = document.querySelector("#inputUp").checked;
-    critNum = document.querySelector("#inputNum").checked;
-    critSpec = document.querySelector("#inputSpec").checked;
-    passLength = document.querySelector("#inputLen").value;
+    setTimeout(function() {
+      critLow = document.querySelector("#inputLow").checked;
+      critUp = document.querySelector("#inputUp").checked;
+      critNum = document.querySelector("#inputNum").checked;
+      critSpec = document.querySelector("#inputSpec").checked;
+      passLength = document.querySelector("#inputLen").value;
+    }, 10)
+    // setTimeout(function, milliseconds, param1, param2, ...)
   }
   else {
     console.log("Invalid input ID provided")
@@ -127,26 +132,27 @@ inputLen.addEventListener("input", inputCheck);
 pCriteria.addEventListener("reset", inputCheck);
 
 // Add event listener to submit form input that will write the password using selected criteria
+writeBtn.addEventListener("click", writePassword);
 
 // Inline HTML
-var markupCriteria = `<form action="#" method="get">
-<input type="checkbox" name="uppercase" value="upper">
-<label for="uppercase">Uppercase</label>
+// var markupCriteria = `<form action="#" method="get">
+// <input type="checkbox" name="uppercase" value="upper">
+// <label for="uppercase">Uppercase</label>
 
-<input type="checkbox" name="numeric" value="number" checked>
-<label for="numeric">Numbers</label><br>
+// <input type="checkbox" name="numeric" value="number" checked>
+// <label for="numeric">Numbers</label><br>
 
 
-<input type="checkbox" name="lowercase" value="lower" checked>
-<label for="lowercase">Lowercase</label>
+// <input type="checkbox" name="lowercase" value="lower" checked>
+// <label for="lowercase">Lowercase</label>
 
-<input type="checkbox" name="special" value="special">
-<label for="special">Symbols</label><br><br>
+// <input type="checkbox" name="special" value="special">
+// <label for="special">Symbols</label><br><br>
 
-<label for="length">Password Length <i>(8-128 characters)</i></label><br>
-<input type="range" id="pLength" name="length" oninput="this.nextElementSibling.value=this.value" min="8" max="128" value="10" step="1">
-<output>10</output><br><br>
+// <label for="length">Password Length <i>(8-128 characters)</i></label><br>
+// <input type="range" id="pLength" name="length" oninput="this.nextElementSibling.value=this.value" min="8" max="128" value="10" step="1">
+// <output>10</output><br><br>
 
-<input type="submit" value="Fill Password">
-<input type="reset" value="Reset">
-</form>`;
+// <input type="submit" value="Fill Password">
+// <input type="reset" value="Reset">
+// </form>`;
